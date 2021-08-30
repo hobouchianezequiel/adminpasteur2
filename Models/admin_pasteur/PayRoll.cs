56612dc1dc72ff475bd9ef85a0e4f559c286b91d
hobouchianezequiel <73626721+hobouchianezequiel@users.noreply.github.com>
2021-08-30 10:54:09 -0300

    .

--- Models/admin_pasteur/PayRoll.cs
+++ Models/admin_pasteur/PayRoll.cs
@@ -13,42 +13,55 @@ namespace delSur_Solutions.Models
         [Key]
         public int Id { get; set; }
 
+        [Display(Name = "AFILIAD@")]
         [JsonProperty("affiliate")]
         public Affiliate Affiliate { get; set; }
 
+        [Display(Name = "PSICOLOG@")]
         [JsonProperty("doctor")]
         public Doctor Doctor { get; set; }
 
+        [Display(Name = "FECHA")]
         [JsonProperty("date")]
         public DateTime Date { get; set; }
 
+        [Display(Name = "COD. PREST.")]
         [JsonProperty("cod_prest")]
         public long CodPrest { get; set; }
 
+        [Display(Name = "TRANS. (*)")]
         [JsonProperty("trans")]
         public string Trans { get; set; }
 
+        [Display(Name = "N° AUTORIZ.")]
         [JsonProperty("authorization_number")]
         public long AuthorizationNumber { get; set; }
 
+        [Display(Name = "DIAGNOSTICO")]
         [JsonProperty("diagnosis")]
         public string Diagnosis { get; set; }
 
+        [Display(Name = "COSEG.")]
         [JsonProperty("coseg")]
         public long Coseg { get; set; }
 
+        [Display(Name = "PERIODO FACTURADO")]
         [JsonProperty("invoiced_period")]
         public DateTime InvoicedPeriod { get; set; }
 
+        [Display(Name = "CONSULTORIO")]
         [JsonProperty("consulting_room")]
         public string ConsultingRoom { get; set; }
 
+        [Display(Name = "OBSERVACIONES")]
         [JsonProperty("observations")]
         public string Observations { get; set; }
 
+        [Display(Name = "IMPORTE")]
         [JsonProperty("amount")]
         public long Amount { get; set; }
-
+        
+        [Display(Name = "TOTAL FACTURADO")]
         [JsonProperty("total_amount")]
         public long TotalAmount { get; set; }
 
